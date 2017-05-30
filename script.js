ee65a27f699b485b23b33f29d5f4dad054d31968
Allison Bessett <allison_17@hotmail.com>
2017-05-30 10:36:07 -0700

    Add stylesheet

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
+
+$(document).ready(function () {
+
+  var base_url = "https://pacific-meadow-64112.herokuapp.com/data-api/";
+  var collection = "abessett";
+  
+});
