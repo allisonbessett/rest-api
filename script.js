5cb9994cd82702135fc3a7b968b5b32fe17ee448
Allison Bessett <allison_17@hotmail.com>
2017-05-30 10:55:08 -0700

    Adding book name functionality

--- script.js
+++ script.js
@@ -15,4 +15,32 @@ $(document).ready(function () {
     bookTable.hide();
   });
   
+  
+   function updateArray(book, author) {
+    arr.push({tBook: book, tAuthor: author});
+  }
+  function updateTable() {
+    $('#bookTable tbody').html('<tr></tr>');
+    for (var i = 0; i < arr.length; i++) {
+    $('#bookTable tBody').append('<tr><td>' + arr[i].tBook + '</td>' + '<td>' + arr[i].tAuthor + '</td>' + '<td>' + "<button type = 'button' id='edit_button'>Edit</button>" + '</td>' + '<td>' + "<button type='button' id = 'delete_button'>Delete</button>" + '</td>' + '</tr>'); 
+    count++;
+    }
+    
+  }
+  
+  $('#submit').on('click', function (e) {
+    e.preventDefault();
+    var book = $('#book').val();
+    var bookName = {
+      book: book
+    };
+    
+    createbookName(bookName);
+    bookTable.show();
+    bookForm.hide();
+  
+  });
+  
+  //------------
+  
 });
