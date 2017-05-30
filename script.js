7aa7ee8b4d7e51c3b491b4fc36ab8edcf254c2ca
Allison Bessett <allison_17@hotmail.com>
2017-05-30 10:42:22 -0700

    Change city table and form to book table and form

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function () {
+  
+  var bookForm = $('#bookForm'), bookTable = $('#bookTable');
 
   var base_url = "https://pacific-meadow-64112.herokuapp.com/data-api/";
   var collection = "abessett";
+  
+  var bookForm = $('#bookForm').hide();
+  
+  $('#addItemButton').on('click', function (e) {
+    bookForm.show();
+    bookTable.hide();
+  });
   
 });
