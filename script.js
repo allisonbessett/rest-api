25f315c27b9f2d755cd4fe06761fe56bcd4adbd1
Allison Bessett <allison_17@hotmail.com>
2017-06-01 12:22:36 -0700

    Working on table

--- script.js
+++ script.js
@@ -22,37 +22,24 @@ $(document).ready(function () {
 
   function updateTable() {
     $('#bookTable tbody').html('<tr></tr>');
-       console.log("working");
-    var newContent = '';
     for (var i = 0; i < arr.length; i++) {
       $('#bookTable tBody').append('<tr><td>' + book + '</td>' + '</tr>'); 
     count++;
-      book.innerHTML = newContent;
     }
   }
-  
-  xhr.open('GET', 'https://pacific-meadow-64112.herokuapp.com/data-api/abessett', true);
-  xhr.send(null);
+
   
   $('#submit').on('click', function (e) {
     e.preventDefault();
     bookForm.hide();
     bookTable.show();
-    var bookInfo = $
+    var bookInfo = book.serialize();
+    $.post('https://pacific-meadow-64112.herokuapp.com/data-api/abessett', bookInfo, function(data) {
+      book.html(data);
     });
-  //------------
-  
-
-  
-  function reportResponse(response) {
-    $('#response').text(JSON.stringify(response, null, 4));
-  }
-  
-  function reportAjaxError(jqXHR, textStatus, errorThrown) {
-    $('#response').text("An error occurred from AJAX.");
-  }
-  
-  function clearResponse() {
-    $('#response').empty();
-  }
+    updateTable();
   });
+  
+});
+  
+  
